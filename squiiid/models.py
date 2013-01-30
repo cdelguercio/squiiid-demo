b55811c8866e1c8a737009890a4a9b67459f3b4c
cdelguercio <cdelguercio@gmail.com>
2013-01-29 17:57:24 -0800

    new styling for demo

--- squiiid/models.py
+++ squiiid/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class SquiiidImage(models.Model):
+    image = models.FileField(upload_to='images')
+    name = models.CharField(max_length=10000)
+    photographer = models.CharField(max_length=10000)
+    date = models.DateTimeField()
+
+    def __unicode__(self):
+        return unicode('')
+
+class Tag(models.Model):
+    image = models.ForeignKey(SquiiidImage)
+    url = models.CharField(max_length=10000)
+    x = models.IntegerField()
+    y = models.IntegerField()
+    date = models.DateTimeField()
+
+    def __unicode__(self):
+        return unicode('')
